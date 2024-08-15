8d20bfbfe4aeaa1f15c1cc5bdb4bb40e989f0513
Sohamjade <157983419+SohamJade@users.noreply.github.com>
2024-08-15 17:23:37 +0530

    Concatenation of a string.cpp

---
+++ Concatenation of a string.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string first_name,last_name;
+    first_name = "Soham";
+    last_name = "Jade";
+
+    cout<<first_name+last_name;
+}
+
+/*
+SohamJade
+*/
